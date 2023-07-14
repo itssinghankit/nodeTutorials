a12faadd6938803288cd5cfe1adc4f0e28eace88
itssinghankit <singhankit.kr@gmail.com>
2023-07-14 21:23:58 +0530

    unsplash project

--- api/server.js
+++ api/server.js
@@ -1,32 +1,37 @@
 const express=require("express");
 const https=require("https");
+const bodyParser=require("body-parser")
 
 const app =express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/",function(req,res){
+    var type=req.body.Iname;
+
+    var url="https://api.unsplash.com/search/photos?&query="+type+"&client_id=q9mt5PO7SFAW9tuYBYScqqQryyKimNnFW0sEsB-O2Ss";
+
+    Rdata="";
+
+    https.get(url,function(response){
+        response.on("data",function(data){
+            Rdata+=data;
+        });
+
+        response.on("end",function(){
+            var parsedData=JSON.parse(Rdata);
+            res.write("<h1>"+parsedData.results[0].alt_description+"</h1>");
+            res.write("<h1>"+parsedData.results[0].description+"</h1>");
+            res.write("<img src="+parsedData.results[0].urls.small+">");
+            res.send();
+        })
+
+    })
+
+})
 
 app.get("/",function(req,res){
-    const url="https://api.unsplash.com/search/photos?&query=android&client_id=q9mt5PO7SFAW9tuYBYScqqQryyKimNnFW0sEsB-O2Ss";
-    https.get(url,function(response){
-        console.log(response.statusCode);
-
-        let responseData="";
-
-        //response comes in chunks
-        response.on("data", function(data) {
-            // Accumulate the received data
-        
-            responseData += data;
-        });
     
-        //when complete response is received then parse
-        response.on("end", function() {
-            // Parse the accumulated data once all data has been received
-            const information = JSON.parse(responseData);
-            // Handle the parsed information as needed
-            console.log(information.results[0].alt_description)
-        });
-
-    });
-    res.send("successful");
+   res.sendFile(__dirname+"/index.html")
 });
 
 app.listen(3000,function(){
