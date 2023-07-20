0d2f536e6fdba3895156abe8e4e1bcac05f47eef
itssinghankit <singhankit.kr@gmail.com>
2023-07-20 17:38:58 +0530

    module making

--- todolist_v1/app.js
+++ todolist_v1/app.js
@@ -1,35 +1,49 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs")
+const date=require(__dirname+"/modulefunc.js")
 
 const app =express();
 
-let items=["Buy Food","Cook Food","Eat Food"]
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItems=[];
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.set("view engine","ejs");
 
-let today=new Date();
+app.get("/",function( req,res ){
 
-let options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-};
+    const day=date.getDate();
 
-let date=today.toLocaleDateString("en-US",options)
-
-app.get("/",function(req,res){
-
-   res.render("list",{kindOfDay:date,listItems:items});
+   res.render("list",{listHeading:day,listItems:items});
 
 });
 
 app.post("/",function(req,res){
+
+if(req.body.button==="Work"){
+    workItems.push(req.body.newItem)
+    res.redirect("/work")
+}else{
    items.push(req.body.newItem);
    res.redirect("/");
+}
+
 });
+
+//for work page
+app.get("/work",function(req,res){
+ 
+res.render("list",{listHeading:"Work",listItems:workItems})
+
+})
+
+//for about page
+app.get("/about",function(req,res){
+    res.render("about")
+})
 
 app.listen(3000,()=>{
     console.log("connected to server");
